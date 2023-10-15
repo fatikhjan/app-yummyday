b2a5baca34d32f8361535bdc7fc923719aa74346
Fotikhjon <muhammad2004fotih@gmail.com>
2023-10-15 19:43:43 +0500

    First commit

---
+++ src/main/java/uz/yummyday/yummyday/YummydayApplication.java
@@ -0,0 +1,13 @@
+package uz.yummyday.yummyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummydayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummydayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/yummyday/yummyday/YummydayApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.yummyday.yummyday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
